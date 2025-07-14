9ef1c1e4db0b35343f2d61b35898160695327f99
Vijay Saini <vijay_saini@optum.com>
2025-07-14 18:05:44 +0530

    change

--- data/database.js
+++ data/database.js
@@ -5,7 +5,8 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+ //const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
@@ -23,3 +24,6 @@ try {
 const database = client.db(dbName);
 
 export default database;
+
+
+
