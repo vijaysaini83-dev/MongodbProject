96a0ee5fce60d2418cd1d14dffb596527ca443d8
Vijay Saini <vijay.saini83@gmail.com>
2025-07-15 23:06:18 +0530

    Testing

--- data/database.js
+++ data/database.js
@@ -17,7 +17,7 @@ try {
   console.log("Connected successfully to server");
 } catch (error) {
   console.log("Connection failed.");
-  console.error(error);
+  //console.error(error);
   await client.close();
   console.log("Connection closed.");
 }
