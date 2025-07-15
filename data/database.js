484ded2f6a04e5cb8e83b2590f39653d3c067fb4
Vijay Saini <vijay.saini83@gmail.com>
2025-07-15 23:24:01 +0530

    Testing

--- data/database.js
+++ data/database.js
@@ -1,54 +1,26 @@
-// import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 
-// const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
-// const dbUser = process.env.MONGODB_USERNAME;
-// const dbPassword = process.env.MONGODB_PASSWORD;
-// const dbName = process.env.MONGODB_DB_NAME;
+const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const dbUser = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DB_NAME;
 
 //const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 // const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
-// const client = new MongoClient(uri);
+const uri = `mongodb+srv://vijaysaini83:Lucky@251001@cluster0.tdqsxds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri);
 
-// console.log("Trying to connect to db");
+console.log("Trying to connect to db");
 
-// try {
-//   await client.connect();
-//   await client.db(dbName).command({ ping: 1 });
-//   console.log("Connected successfully to server");
-// } catch (error) {
-//   console.log("Connection failed.");
-//   await client.close();
-//   console.log("Connection closed.");
-// }
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://vijaysaini83:Lucky@251001@cluster0.tdqsxds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+try {
+  await client.connect();
+  await client.db(dbName).command({ ping: 1 });
+  console.log("Connected successfully to server");
+} catch (error) {
+  console.log("Connection failed.");
+  await client.close();
+  console.log("Connection closed.");
 }
-run().catch(console.dir);
 
 const database = client.db(dbName);
 
