b58e827638a0a25e982fa477768901756714841a
Vijay Saini <vijay.saini83@gmail.com>
2025-07-15 22:48:02 +0530

    Testing

--- data/database.js
+++ data/database.js
@@ -1,29 +1,26 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
- //const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+//const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
 
-console.log('Trying to connect to db');
+console.log("Trying to connect to db");
 
 try {
   await client.connect();
   await client.db(dbName).command({ ping: 1 });
-  console.log('Connected successfully to server');
+  console.log("Connected successfully to server");
 } catch (error) {
-  console.log('Connection failed.');
+  console.log("Connection failed.");
   await client.close();
-  console.log('Connection closed.');
+  console.log("Connection closed.");
 }
 
 const database = client.db(dbName);
 
 export default database;
-
-
-
