2f61c26b0b990d7a5c57b1de63d72d5a118bfdef
Vijay Saini <vijay.saini83@gmail.com>
2025-07-15 23:04:42 +0530

    Testing

--- data/database.js
+++ data/database.js
@@ -5,8 +5,8 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-//const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
 
 console.log("Trying to connect to db");
